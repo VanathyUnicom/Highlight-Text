69b19d61d01a6945ed7320971251cb16cc2d4ca2
Vanathy <vanathy@gmail.com>
2023-04-30 22:27:02 -0700

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './Component/customer/customer.component';
+import { OrderComponent } from './Component/order/order.component';
+import { HighlightPipe } from './pipe/highlight.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OrderComponent,
+    HighlightPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pipe/highlight.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'highlight'
+})
+export class HighlightPipe implements PipeTransform {
+
+  transform(wholeText: string, searchQuery: string): string {
+    if (!searchQuery) {
+      return wholeText;
+    }
+    const re = new RegExp(searchQuery, 'gi');
+    return wholeText.replace(re,  '<mark>$&</mark>');
+  }
+
+}
